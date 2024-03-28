644dc282b8b139ae9b4fa9753db34f4e67fa5190
Sarthak Nandedkar <sarthakn386@gmail.com>
2024-03-28 16:18:58 +0530

    updated patient controller

--- routes/patients.js
+++ routes/patients.js
@@ -1,13 +1,19 @@
-import express from 'express';
-import { createNewPatient, deleteById, getAllPatient, getPatientById, updateById } from '../controllers/patient.controller.js';
-import doctorMiddleware from '../middleware/isDoc.js';
+import express from "express";
+import {
+  createNewPatient,
+  deleteById,
+  getAllPatient,
+  getPatientById,
+  updateById,
+} from "../controllers/patient.controller.js";
+import doctorMiddleware from "../middleware/isDoc.js";
 
 const router = express.Router();
 
-router.route('/register').post(createNewPatient);
-router.route('/').get(doctorMiddleware,getAllPatient);
-router.route('/:id').get(getPatientById);
-router.route('/update/:id').put(updateById);
-router.route('/delete/:id').delete(deleteById);
+router.route("/register").post(createNewPatient);
+router.route("/").get(getAllPatient);
+router.route("/:id").get(getPatientById);
+router.route("/update/:id").put(updateById);
+router.route("/delete/:id").delete(deleteById);
 
 export default router;
