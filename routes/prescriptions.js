506f7c5c00a9d0a982071725cf18b8ba40445b0f
Sarthak Nandedkar <sarthakn386@gmail.com>
2024-03-28 16:16:19 +0530

    updated prescription route

--- routes/prescriptions.js
+++ routes/prescriptions.js
@@ -5,12 +5,13 @@ import {
   getAllPrescription,
   getPrecrptionById,
 } from "../controllers/prescription.controller.js";
+import doctorMiddleware from "../middleware/isDoc.js";
 
 const router = express.Router();
 
-router.route("/").get(getAllPrescription);
-router.route("/:patientId").get(getPrecrptionById);
-router.route("/add/:patientId").post(addPrescription);
-router.route("/delete/:id").delete(deletePrescriptionById);
+router.route("/").get(doctorMiddleware,getAllPrescription);
+router.route("/:patientId").get(doctorMiddleware,getPrecrptionById);
+router.route("/add/:patientId").post(doctorMiddleware,addPrescription);
+router.route("/delete/:id").delete(doctorMiddleware,deletePrescriptionById);
 
 export default router;
